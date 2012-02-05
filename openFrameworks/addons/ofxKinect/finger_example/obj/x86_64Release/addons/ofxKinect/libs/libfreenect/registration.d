9d053b7611d5c76b3c5ead6ba763312c726dabc3
Ryan Cheu <ryan@ryan-HP-Pavilion-dv7-Notebook-PC.(none)>
2012-02-05 03:08:59 -0500

    MOREEEEE

---
+++ openFrameworks/addons/ofxKinect/finger_example/obj/x86_64Release/addons/ofxKinect/libs/libfreenect/registration.d
@@ -0,0 +1,23 @@
+obj/x86_64Release/addons/ofxKinect/libs/libfreenect/registration.d: \
+ ../../../addons/ofxKinect/libs/libfreenect/registration.c \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect.h \
+ ../../../addons/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h \
+ ../../../addons/ofxKinect/libs/libfreenect/freenect_internal.h \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect.h \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect-registration.h \
+ ../../../addons/ofxKinect/libs/libfreenect/usb_libusb10.h \
+ ../../../addons/ofxKinect/libs/libfreenect/registration.h
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect.h:
+
+../../../addons/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h:
+
+../../../addons/ofxKinect/libs/libfreenect/freenect_internal.h:
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect.h:
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect-registration.h:
+
+../../../addons/ofxKinect/libs/libfreenect/usb_libusb10.h:
+
+../../../addons/ofxKinect/libs/libfreenect/registration.h:
---
+++ openFrameworks/addons/ofxKinect/obj/x86_64Release/addons/ofxKinect/libs/libfreenect/tilt.d
@@ -0,0 +1,20 @@
+obj/x86_64Release/addons/ofxKinect/libs/libfreenect/tilt.d: \
+ ../../../addons/ofxKinect/libs/libfreenect/tilt.c \
+ ../../../addons/ofxKinect/libs/libfreenect/freenect_internal.h \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect.h \
+ ../../../addons/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect-registration.h \
+ ../../../addons/ofxKinect/libs/libfreenect/libfreenect.h \
+ ../../../addons/ofxKinect/libs/libfreenect/usb_libusb10.h
+
+../../../addons/ofxKinect/libs/libfreenect/freenect_internal.h:
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect.h:
+
+../../../addons/ofxKinect/libs/libusb/osx/include/libusb-1.0/libusb.h:
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect-registration.h:
+
+../../../addons/ofxKinect/libs/libfreenect/libfreenect.h:
+
+../../../addons/ofxKinect/libs/libfreenect/usb_libusb10.h:
